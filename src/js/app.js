52063db7a7194fbe8f1088d9c6d7c19c57380481
Милинцевич Михаил <mmilincevich@sinergo.ru>
2018-11-20 17:35:59 +0700

    решен вопрос генерации точек

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-const GALAXY = d3.select('#galaxy');
+const galaxy = d3.select('#galaxy');
 
 let total = 244;
 let data = [
@@ -9,79 +9,24 @@ let data = [
     { name: 'COUNTRY', value: 93 }
 ];
 
-GALAXY.select('g.main')
-    .append('circle')
-    .attr('cx', GALAXY.attr('width')/2)
-    .attr('cy', GALAXY.attr('height')/2)
-    .attr('r', 29)
-    .attr('fill', '#228AA2')
-
-GALAXY.selectAll('g.data')
+galaxy.selectAll('g')
     .data(data)
+    .enter()
     .append('circle')
-    .attr('r', d => calcPercent(d.value))
-    .attr('cx', (d,i) => geneneratePostion(i, d.value).x)
-    .attr('cy', (d,i) => geneneratePostion(i, d.value).y)
-    .attr('fill', 'green')
+    .attr('r', 10)
+    .attr('cy', (d, i) => geneneratePostion(i).y)
+    .attr('cx', (d, i) => geneneratePostion(i).x)
 
-function setPosition(radius) {
-    let maxWidth = GALAXY.attr('width');
-    let maxHeight = GALAXY.attr('height');
-    let pos = {};
-    let genTry = 10;
-
-    if(positions.length) {
-        do {
-            pos.x = geneneratePostion().x
-            pos.y = geneneratePostion().y
-            pos.r = radius
-        } while (overlap(pos) || genTry == 0) {
-            pos.x = geneneratePostion().x
-            pos.y = geneneratePostion().y
-            genTry--
-        }
-    }
-
-
-    function overlap(o) {
-        let x1 = o.x,
-            y1 = o.y,
-            r1 = o.r
-
-        positions.forEach(el => {
-            let x2 = el.x,
-                y2 = el.y,
-                r2 = el.r
-            
-            let dist = ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ (1 / 2);
-
-            if (dist < (r1 + r2)) {
-                return true
-            } else {
-                return false
-            }
-        })
-    }
-
-    return pos
-};
-
-function calcPercent(value) {
-    let total_percent = 100 / (total / value);
-    let visual_percent = 29 / total_percent;
-    
-    return visual_percent;
-}
 
 function geneneratePostion(i, r) {
     let pos = {};
-    let angle = data.length * i + 1 * Math.PI;
-    let radius = calcPercent(total, r);
-    let centerX = GALAXY.attr('width') / 2;
-    let centerY = GALAXY.attr('height') / 2;
+    let parrentR = 29;
+    let angle = i + 1 * Math.PI;
+    let width = galaxy.attr('width') - parrentR*2
+    let height = galaxy.attr('height') - parrentR*2
 
-    pos.x = (centerY + centerX) * Math.sin(angle);
-    pos.y = (centerY + centerX) * Math.cos(angle);
+    pos.x = (parrentR + width / 2 + height / 2 * Math.sin(angle)) 
+    pos.y = (parrentR + height / 2 + width / 2 * Math.cos(angle))
 
     return pos;
 }
